8366bef8852a2b24288b75f1eaac5215d4e61c8a
shivransai <31769511+shivransai@users.noreply.github.com>
2020-04-26 18:47:19 -0400

    Update simulation.m

--- MatLab/simulation.m
+++ MatLab/simulation.m
@@ -9,15 +9,13 @@ for i = 1:828
     x_cord = data(i, 1);
     y_cord = data(i, 2);
     val = data(i, 3);
-    %fprintf("%d %d %d %d \n", x_cord, y_cord, val,i);
     virus_spread(x_cord, y_cord) = val;
     gaussian_filter = fspecial('gaussian', [100,100], 25);
     density = imfilter(virus_spread, gaussian_filter, 'replicate');
   
-    omask = heatmap_overlay(img, density, 'jet');
-    imshow(omask, []);
-    %pause(0.01)
-
+    overlay = heatmap_overlay(img, density, 'jet');
+    imshow(overlay, []);
 end
+
 colormap(jet);     
 colorbar;
